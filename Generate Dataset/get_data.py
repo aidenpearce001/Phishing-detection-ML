8f41f3db77b93b4b11eb95676b4d7730c546f9a4
unknown <aidenpearcewd01@gmail.com>
2021-06-27 15:19:03 +0700

    'update'

---
+++ Generate Dataset/get_data.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+import requests
+
+blacklist = [
+    "https://phishingreel.io/api/v1/panels/today",
+    "https://api.chongluadao.vn/v1/blacklist"
+]
+
+whilelist = [
+    "https://api.chongluadao.vn/v1/whitelist"
+]
+
